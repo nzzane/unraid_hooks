557e904ef81342587551a28ad48f138b32788717
Zane <github@turtledev.nz>
2023-01-22 08:14:08 +1300

    Create VM-NAME-stop.sh

---
+++ VM-NAME-stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nvidia-smi -i 0 -pm 1 &
+sleep 1
+
+#/usr/local/sbin/rc.tweaks set_governor ondemand &
+bash -c 'for ((i=8;i<12;i++)); do cpufreq-set -c $i -g powersave; done'
+bash -c 'for ((i=18;i<24;i++)); do cpufreq-set -c $i -g powersave; done'
+sleep 1
+
+exit 0;
